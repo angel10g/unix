5eeb9cf39fe96c38c3e4df5c0f78fdf36871ea1b
angel10g <162932996+angel10g@users.noreply.github.com>
2025-01-20 23:18:10 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-function guess_files {
-    local file_count=$(ls -1 | wc -l) # Compte le nombre de fichiers dans le répertoire actuel
-    while true; do
-        echo "Combien de fichiers se trouvent dans le répertoire actuel ?"
-        read guess
-        if [[ $guess -eq $file_count ]]; then
-            echo "Félicitations ! Vous avez trouvé le bon nombre : $file_count."
-            break
-        elif [[ $guess -lt $file_count ]]; then
-            echo "C'est trop bas. Essayez encore."
-        else
-            echo "C'est trop haut. Essayez encore."
-        fi
-    done
+echo "[Welcome to Guessing game v0.1]"
+
+function ask {
+	echo "Please enter the number of files in the current directory:"
+	read guess
+    files=$(ls -1 | wc -l)
 }
 
-# Appel de la fonction
-guess_files
+ask
+
+while [[ $guess -ne $files ]]
+do
+	if [[ $guess -lt $files ]] 
+	then
+		echo "Too low."
+	else
+		echo "Too high."
+	fi
+	ask
+done
+
+echo "Well done! It is the correct answer, here is the list of files:"
+echo "---" && ls -1
